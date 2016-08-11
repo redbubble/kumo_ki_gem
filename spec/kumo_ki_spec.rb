646df4f2a3fbaf9446de1507d30150c1dbfe5041
Pat Leamon <patrick@redbubble.com>
2016-08-11 12:16:01 +1000

    Add a specific error message for missing credentials

--- spec/kumo_ki_spec.rb
+++ spec/kumo_ki_spec.rb
@@ -25,6 +25,14 @@ describe KumoKi::KMS do
 
         expect { subject }.to raise_error(KumoKi::DecryptionError, "There was a problem decrypting your secrets: #{nested_error_message}")
       end
+
+      context "missing credentials" do
+        before { allow(kms).to receive(:decrypt).and_raise(Aws::Errors::MissingCredentialsError, "No credentials sucker.")  }
+
+        it "gives a nice message" do
+          expect { subject }.to raise_error(KumoKi::KumoKiError, "No AWS credentials found.  Try setting AWS_SECRET_ACCESS_KEY and AWS_ACCESS_KEY_ID environment variables")
+        end
+      end
     end
 
   end
